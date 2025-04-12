cdbb3d33abdb27905f67a75fde8c5e0e6670b481
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-04-12 23:57:08 +0700

    [JH-04] Build Job component

---
+++ src/components/Job.vue
@@ -0,0 +1,38 @@
+    <script setup>
+    import { defineProps, ref, computed } from 'vue';
+
+    defineProps({
+        job: Object
+    })
+
+</script>
+
+<template>
+    <div class="bg-white rounded-xl shadow-md">
+        <div class="p-4">
+            <div class="mb-6">
+                <div class="text-gray-600 my-2">{{ job.type }}</div>
+                <h3 class="text-xl font-bold">{{ job.title }}</h3>
+            </div>
+
+            <div class="mb-5 line-clamp-4 overflow-hidden" :title="job.description">
+                {{ job.description }}
+            </div>
+
+            <h3 class="text-blue-500 mb-2">{{ job.salary }}/ Year</h3>
+
+            <div class="border border-gray-100 mb-5"></div>
+
+            <div class="flex flex-col lg:flex-row justify-between mb-4">
+                <div class="text-blue-500 mb-3">
+                    <i class="pi pi-map-marker text-blue-700"></i>
+                    {{ job.location }}
+                </div>
+                <a :href="`/job/${job.id}`"
+                    class="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm">
+                    Read More
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
