80e38c468ffeb4bce834dc8272324db90ffad420
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-04-13 13:36:14 +0700

    [[JH-07] Build Homepage

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { DoubleCards, Hero, JobList } from '@/components';
+</script>
+
+<template>
+    <Hero />
+    <DoubleCards />
+    <JobList :limit="3" :showButton="true" />
+</template>
