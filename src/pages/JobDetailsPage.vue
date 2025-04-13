04e37b0aca2eabd521f1b3092fd1313129b9a16f
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-04-13 22:50:01 +0700

    [JH-13] Implement api - Get job details

--- src/pages/JobDetailsPage.vue
+++ src/pages/JobDetailsPage.vue
@@ -1,16 +1,41 @@
-<script setup></script>
+<script setup>
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+import { RouterLink, useRoute } from 'vue-router';
+
+
+const job = ref({});
+const isLoading = ref(true);
+
+const route = useRoute();
+const jobId = route.params.id;
+
+onMounted(async () => {
+    try {
+        const res = await axios.get(`http://localhost:5000/jobs/${jobId}`);
+        if (res.data) job.value = res.data;
+
+    } catch (error) {
+        console.log(error)
+    } finally {
+        isLoading.value = false;
+    }
+
+})
+
+</script>
 
 <template>
-    <section class="bg-blue-50">
+    <section v-if="!isLoading" class="bg-blue-50">
         <div class="container m-auto py-10">
             <div class="grid grid-cols-1 lg:grid-cols-70/30 w-full gap-6">
                 <main>
                     <div class="bg-white p-6 rounded-lg shadow-md text-center lg:text-left">
-                        <div class="text-blue-500 mb-4">Full-Time</div>
-                        <h1 class="text-3xl font-bold mb-4">Senior Vue Developer</h1>
+                        <div class="text-blue-500 mb-4">{{ job.type }}</div>
+                        <h1 class="text-3xl font-bold mb-4">{{ job.title }}</h1>
                         <div class="text-blue-500 mb-4 flex align-middle justify-center lg:justify-start">
                             <i class="pi pi-map-marker text-blue-700 mt-1 mr-2"></i>
-                            <p class="text-blue-700">Boston, MA</p>
+                            <p class="text-blue-700">{{ job.location }}</p>
                         </div>
                     </div>
 
@@ -20,15 +45,12 @@
                         </h3>
 
                         <p class="mb-4">
-                            We are seeking a talented Front-End Developer to join our team
-                            in Boston, MA. The ideal candidate will have strong skills in
-                            HTML, CSS, and JavaScript, with experience working with modern
-                            JavaScript frameworks such as Vue or Angular.
+                            {{ job.description }}
                         </p>
 
                         <h3 class="text-blue-800 text-lg font-bold mb-2">Salary</h3>
 
-                        <p class="mb-4">$70k - $80K / Year</p>
+                        <p class="mb-4">{{ job.salary }} / Year</p>
                     </div>
                 </main>
 
@@ -38,13 +60,10 @@
                     <div class="bg-white p-6 rounded-lg shadow-md">
                         <h3 class="text-xl font-bold mb-6">Company Info</h3>
 
-                        <h2 class="text-2xl">NewTek Solutions</h2>
+                        <h2 class="text-2xl">{{ job.company.name }}</h2>
 
                         <p class="my-2">
-                            NewTek Solutions is a leading technology company specializing in
-                            web development and digital solutions. We pride ourselves on
-                            delivering high-quality products and services to our clients
-                            while fostering a collaborative and innovative work environment.
+                            {{ job.company.description }}
                         </p>
 
                         <hr class="my-4" />
@@ -52,20 +71,21 @@
                         <h3 class="text-xl">Contact Email:</h3>
 
                         <p class="my-2 bg-blue-100 p-2 font-bold">
-                            contact@newteksolutions.com
+                            {{ job.company.contactEmail }}
                         </p>
 
                         <h3 class="text-xl">Contact Phone:</h3>
 
-                        <p class="my-2 bg-blue-100 p-2 font-bold">555-555-5555</p>
+                        <p class="my-2 bg-blue-100 p-2 font-bold">{{ job.company.contactPhone }}</p>
                     </div>
 
                     <!-- Manage -->
                     <div class="bg-white p-6 rounded-lg shadow-md mt-6">
                         <h3 class="text-xl font-bold mb-6">Manage Job</h3>
-                        <a href="add-job.html"
-                            class="bg-blue-500 hover:bg-blue-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">Edit
-                            Job</a>
+                        <RouterLink :to="`/jobs/edit/${job.id}`"
+                            class="bg-blue-500 hover:bg-blue-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                            Edit
+                            Job</RouterLink>
                         <button
                             class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                             Delete Job
@@ -75,4 +95,7 @@
             </div>
         </div>
     </section>
+    <div v-else class="text-center text-gray-500 py-6">
+        <PulseLoader />
+    </div>
 </template>
