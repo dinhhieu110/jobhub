15edf68ad99ee35a1795186fbc362569b78dd2de
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-04-13 19:05:00 +0700

    [JH-10] Build JobDetails page

--- src/components/Job.vue
+++ src/components/Job.vue
@@ -28,7 +28,7 @@
                     <i class="pi pi-map-marker text-blue-700"></i>
                     {{ job.location }}
                 </div>
-                <RouterLink :to="`/job/${job.id}`"
+                <RouterLink :to="`/jobs/${job.id}`"
                     class="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm">
                     Read More
                 </RouterLink>
